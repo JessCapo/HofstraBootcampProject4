db1b2ac7f541b09c7cb3e5a5c257145d141519a0
JessCapo <jlcapob@gmail.com>
2025-11-09 17:17:49 -0500

    added uri for mongodb in separate .env file

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-import React, { useState, useEffect, useCallback } from 'react';
-// Firebase Imports for React environment
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInAnonymously, signInWithCustomToken, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore, doc, setDoc, Timestamp } from 'firebase/firestore';
-
-// Global Variables
-const appId = typeof __app_id !== 'undefined' ? __app_id : 'default-app-id';
-const firebaseConfig = typeof __firebase_config !== 'undefined' ? JSON.parse(__firebase_config) : null;
-const initialAuthToken = typeof __initial_auth_token !== 'undefined' ? __initial_auth_token : null;
-const apiKey = ""; // Gemini API Key
+mongodb+srv://jlcapob_db_user:NXxJKow1lZF151tw@cluster0.qiy1ipn.mongodb.net/?appName=Cluster0

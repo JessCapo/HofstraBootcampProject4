5b0034bbcf67d11430e7b20f21f097c89da5bc53
JessCapo <jlcapob@gmail.com>
2025-11-09 16:59:29 -0500

    added Geminini api key, will enable search for info on dog behavior

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInAnonymously, signInWithCustomToken, onAuthStateChanged } from 'firebase/auth';
 import { getFirestore, doc, setDoc, Timestamp } from 'firebase/firestore';
+
+// Global Variables
+const appId = typeof __app_id !== 'undefined' ? __app_id : 'default-app-id';
+const firebaseConfig = typeof __firebase_config !== 'undefined' ? JSON.parse(__firebase_config) : null;
+const initialAuthToken = typeof __initial_auth_token !== 'undefined' ? __initial_auth_token : null;
+const apiKey = ""; // Gemini API Key

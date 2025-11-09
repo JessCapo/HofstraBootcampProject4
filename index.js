aca5940b5cb54701780be25dee591d15cc95aabd
JessCapo <jlcapob@gmail.com>
2025-11-09 17:20:49 -0500

    added environmental variables and access to uri

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-mongodb+srv://jlcapob_db_user:NXxJKow1lZF151tw@cluster0.qiy1ipn.mongodb.net/?appName=Cluster0
+// index.js - runs the backend API
+
+// environment variables
+require('dotenv').config();
+
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors'); // Connects to React app
+
+// Access to URI
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3000;
+const DB_NAME = 'behavioral_db'; 
+
+const app = express();
